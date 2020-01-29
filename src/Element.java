99328654240376fcfb3ae4033a04c8497e648571
danfuz <danryafuz@gmail.com>
2020-01-29 13:44:04 -0500

    n

--- src/Element.java
+++ src/Element.java
@@ -1,10 +1,28 @@
+
 public class Element {
     private double weight, value;
     private String name;
- 
+
     public Element(String name, double weight, double value) {
         this.weight = weight;
         this.value = value;
         this.name = name;
+    }
+    public Element(Element orig) {
+        this.weight = orig.weight;
+        this.value = orig.value;
+        this.name = orig.name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/ElementParser.java
+++ src/ElementParser.java
@@ -3,46 +3,50 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ElementParser {
-    private int weightM;
-    private ArrayList<Element> ele;
-    public ElementParser(int weightMax){
-        weightM = weightMax;
-        ele = new ArrayList<>();
 
-    }
- 
-    public ArrayList<String> readElements(){
-        try{
-            File file = new File("./src/Elements.txt");
-            Scanner reader = new Scanner(file);
- 
-            ArrayList<String> list = new ArrayList<>();
-            while(reader.hasNextLine()){
-                String s = reader.nextLine();
-                list.add(s);
-            }
-            System.out.println(list);
-            return list;
- 
-        }catch(Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
- 
-    public static void main(String[] args) {
-        ElementParser e = new ElementParser(200);
-        ArrayList<String> list = e.readElements();
-        ArrayList<Element> lel = new ArrayList<>();
-        for(String s: list){
+    public static Element[] elements;
+
+    static {
+        ArrayList<String> list = readElements();
+        elements = new Element[100];
+        int i = 0;
+        for (String s : list) {
             String[] parts = s.split(" ");
             String name = parts[0];
             double weight = Double.parseDouble(parts[1]);
             double value = Double.parseDouble(parts[2]);
             Element ele = new Element(name, weight, value); //Make a simple Element class.
-            lel.add(ele);
+            elements[i] = ele;
+            i++;
         }
     }
- 
- 
+
+    private static ArrayList<String> readElements() {
+        try {
+            File file = new File("./src/BackPackProblem/Elements.txt");
+            Scanner reader = new Scanner(file);
+
+            ArrayList<String> list = new ArrayList<>();
+            while (reader.hasNextLine()) {
+                String s = reader.nextLine();
+                list.add(s);
+            }
+            System.out.println(list);
+            return list;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Element[] getElements() {
+        Element[] copy = new Element[elements.length];
+        int i = 0;
+        for (Element orig : elements) {
+            copy[i] = new Element(orig);
+            i++;
+        }
+        return copy;
+    }
 }
