0433a53402d4b2b775b2aca02b627137cb6c5adc
danfuz <danryafuz@gmail.com>
2020-01-29 13:45:58 -0500

    n

---
+++ src/Elements.java
@@ -0,0 +1,28 @@
+public class Elements {
+//    TO GET THE ELEMENTS:
+//
+//    Element[] elements = ElementParser.getElements();
+//
+//    Individual.
+//    What is an "individual" in this project?  How should we save the gene?
+//    A backpack.                            An array of booleans or ints.
+//
+//    How can we score the "fitness" of an individual?
+//    The sum of the values.
+//    With a heavy penalty for going over 200kg...
+//    Maybe if(weight>200): -100*(weight-200)
+//
+//    How can you mutate the genes?
+//    Randomly turn them on or off.
+//
+//    Does mating make sense here?  If so, how can you mate 2 individuals?
+//    It can.  Maybe take the first half of one individual +
+//    second half of the other, and vice versa.  Will create
+//              2 offspring.
+//
+//
+//            Population.
+
+
+
+}
