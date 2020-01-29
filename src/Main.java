1f78829e80be516979db21af04b7ff55522be9f2
zachm7115 <zachm7115@gmail.com>
2020-01-29 14:20:28 -0500

    Element "S" class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
 
+    public static void main(String[] args) {
+        Population pop = new Population(100, 0.5, 1);
+
+        for (int i = 0; i < 10; i++) {
+            pop.kill();
+            pop.regenerate();
+        }
+
+    }
+
 }
